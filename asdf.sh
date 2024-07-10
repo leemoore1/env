9965802059e23eb3d7baf09041cc410e17f3813f
Lee Moore <lee@wickedrebel.dev>
2024-07-10 22:00:17 +0100

    Fixed some issues

--- asdf.sh
+++ asdf.sh
@@ -5,31 +5,35 @@ sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-source "$HOME/.asdf/asdf.sh"
+. "$HOME/.asdf/asdf.sh"
 
 ## asdf plugins
 
-### langs
-asdf plugin-add packer https://github.com/asdf-community/asdf-hashicorp.git
+### HashiCorp
 asdf plugin-add consul https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add packer https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add vault https://github.com/asdf-community/asdf-hashicorp.git
+
+asdf install consul latest && asdf global packer latest
+asdf install packer latest && asdf global packer latest
+asdf install terraform latest && asdf global terraform latest
+asdf install vault latest && asdf global vault latest
+
 asdf plugin-add terragrunt https://github.com/ohmer/asdf-terragrunt
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf plugin-add spring-boot https://github.com/joschi/asdf-spring-boot
 
-### misc
+### Miscellaneous
 asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git
 asdf plugin-add maven https://github.com/halcyon/asdf-maven
 asdf plugin-add pnpm https://github.com/jonathanmorley/asdf-pnpm
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 asdf plugin-add tflint https://github.com/skyzyx/asdf-tflint
-asdf plugin-add vault https://github.com/asdf-community/asdf-hashicorp.git
 
 asdf install pnpm latest && asdf global pnpm latest
-asdf install terraform latest && asdf global terraform latest
 asdf install tflint latest && asdf global tflint latest
-asdf install vault latest && asdf global vault latest
 
-### k8s & helm
+### Kubernetes & Tools
 asdf plugin-add k9s https://github.com/looztra/asdf-k9s
 asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
 asdf plugin-add kubie https://github.com/johnhamelink/asdf-kubie
--- setup-extended.sh
+++ setup-extended.sh
@@ -14,11 +14,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Update Oh My Zsh plugin list
-sed -i 's/plugins=(git)/plugins=(plugins=(ansible asdf aws docker dotnet git golang helm kubectl node npm minikube python pip terraform yarn zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(ansible asdf aws docker dotnet git golang helm kubectl node npm minikube python pip terraform yarn zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 
 ## Oh My Zsh theme
 $ git clone https://github.com/moarram/headline.git $ZSH_CUSTOM/themes/headline
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="headline"/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="headline/headline"/g' ~/.zshrc
 
 # Set default shell to ZSH
 sudo chsh -s /usr/bin/zsh $USER
